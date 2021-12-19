b7a09fb2bf242542d5b17f9a2baa1e87477c969f
wienerdev <matheuswiener9@gmail.com>
2021-12-19 13:25:04 -0300

    Telas relacionadas a viagens implementadas / Correção de bugs

--- app/src/main/java/com/example/speedcarv2/ViagemRealizadaActivity.kt
+++ app/src/main/java/com/example/speedcarv2/ViagemRealizadaActivity.kt
@@ -1,11 +1,65 @@
 package com.example.speedcarv2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 
 class ViagemRealizadaActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_viagem_realizada)
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        val navView = findViewById<NavigationView>(R.id.navView)
+
+        // Menu Hamburguer
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.contaUsuario -> Toast.makeText(applicationContext, "Clicou em Conta",
+                    Toast.LENGTH_SHORT).show()
+
+                R.id.viagens -> Toast.makeText(applicationContext, "Clicou em Viagens",
+                    Toast.LENGTH_SHORT).show()
+
+                R.id.viagensRealizadas -> Toast.makeText(applicationContext, "Clicou em Viagens Realizadas",
+                    Toast.LENGTH_SHORT).show()
+
+                R.id.pagamentos -> Toast.makeText(applicationContext, "Clicou em Pagamentos",
+                    Toast.LENGTH_SHORT).show()
+
+                R.id.logout -> logout()
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun logout() {
+        FirebaseAuth.getInstance().signOut()
+        startActivity(Intent(this@ViagemRealizadaActivity, LoginActivity::class.java));
+        finish();
     }
 }
